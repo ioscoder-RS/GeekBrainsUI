bf3ca349a0a112d9d30bef5dcbecf098ad2d2fc4
Sergei Raskin <ioscoder@yandex.ru>
2019-12-16 16:50:52 +0300

    дз по урокам 6 и 7 только обяз. пункты

---
+++ lesson07/GeekbrainsUI/Model/Group.swift
@@ -0,0 +1,15 @@
+//
+//  Group.swift
+//  GeekbrainsUI
+//
+//  Created by raskin-sa on 08/12/2019.
+//  Copyright © 2019 raskin-sa. All rights reserved.
+//
+
+import Foundation
+
+struct Group{
+    var groupName:String
+    var avatarPath:String
+    var id: Int
+}
