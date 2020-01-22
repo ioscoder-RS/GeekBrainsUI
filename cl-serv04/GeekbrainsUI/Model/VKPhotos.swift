f3025443f70ef690ffc04acbcde86f00c0b5e48e
Sergei Raskin <ioscoder@yandex.ru>
2020-01-22 17:09:55 +0300

    Версия с объектами realm

---
+++ cl-serv04/GeekbrainsUI/Model/VKPhotos.swift
@@ -0,0 +1,46 @@
+//
+//  VKPhotos.swift
+//  GeekbrainsUI
+//
+//  Created by raskin-sa on 20/01/2020.
+//  Copyright © 2020 raskin-sa. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Item
+struct VKPhoto: Codable {
+    let id, albumID, ownerID: Int
+    let sizes: [Size]
+   // let smallPhoto, midPhoto, bigPhoto: String
+    let text: String
+    let date: Int
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case albumID = "album_id"
+        case ownerID = "owner_id"
+        case sizes
+        case text, date
+    }
+}
+
+// MARK: - Size
+struct Size: Codable {
+    let type: String
+    let url: String
+    let width, height: Int
+}
+
+// MARK: - Welcome
+struct PhotosResponse: Codable {
+    let response: PhotosResponseData
+}
+
+// MARK: - Response
+struct PhotosResponseData: Codable {
+    let count: Int
+    let items: [VKPhoto]
+}
+
+var myPhotos = [VKPhoto]()
